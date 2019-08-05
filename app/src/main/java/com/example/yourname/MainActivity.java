f0b13596ff3fce3b2a8c3ddf3d3951f615a650f2
taruc <chewws-wm17@student.tarc.edu.my>
2019-08-05 14:39:48 +0800

    commit

---
+++ app/src/main/java/com/example/yourname/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.yourname;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Intent SPM;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /*Button button = findViewById(R.id.button);
+        View button2;
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Class<?> activity_main2;
+                startActivity(new Intent(MainActivity.this, SPM));
+            }
+
+    }
+
+        Button button2 = findViewById(R.id.button);
+        View button3;
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Class<?> activity_main2;
+                startActivity(new Intent(MainActivity.this, OLevel));
+            }
+
+        }
+        Button button3 = findViewById(R.id.button);
+        View button4;
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Class<?> activity_main2;
+                startActivity(new Intent(MainActivity.this, UEC));
+            }
+
+        }
+        Button button4 = findViewById(R.id.button);
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Class<?> activity_main2;
+                startActivity(new Intent(MainActivity.this, SPMDiploma));
+            }
+
+        }*/
+
+
+    }}
+
+
